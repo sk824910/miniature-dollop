ac572359fba7dadc2851adcfdc17e06a81e79dd9
steve <47872048+sk824910@users.noreply.github.com>
2023-01-19 20:16:06 -0500

    adding primeNg

--- apps/minidollop-ui/src/app/app.component.ts
+++ apps/minidollop-ui/src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PrimeNGConfig } from "primeng/api";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'minidollop-ui';
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) {}
 
+  title = "minidollop-ui";
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
   log() {
-    console.log('here is some log');
+    console.log("here is some log");
   }
 }
